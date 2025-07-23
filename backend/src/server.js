3dc421eacfd0ef47773415e8d8e2e450ea750b2f
Jai Akash <jaiakash0117@gmail.com>
2025-07-23 17:03:30 +0530

    Updated for Deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors"
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
@@ -10,11 +11,12 @@ dotenv.config();
 
 const app=express();
 const PORT=process.env.PORT || 5001
+const __dirname = path.resolve();
 
 //Middleware - Used for Autentication check
 app.use(express.json());
 
-app.use(cors());
+if(process.env.NODE_ENV !== "production") {app.use(cors()); }
 
 app.use((req,res,next)=> {
     console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
@@ -26,10 +28,18 @@ app.use(rateLimiter);
 
 app.use("/api/notes",notesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+    });
+}
+
+
 connectDB().then(()=> {
     app.listen(PORT, ()=> {
     console.log("Port Started in ",PORT);
 });
 })
-
 
